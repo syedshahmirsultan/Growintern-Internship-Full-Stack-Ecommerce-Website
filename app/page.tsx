d2137f4b2c6869b7af92aa41cfed888dac3f4933
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-10 21:43:29 +0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
 
   return (
-    <div></div>
+    <div>
+      <h1 className="flex justify-center items-center text-2xl txet-gray-950 font-bold">Home</h1>
+    </div>
   )
 
 }
