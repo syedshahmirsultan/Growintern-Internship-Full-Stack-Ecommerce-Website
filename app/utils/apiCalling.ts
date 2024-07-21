e633cbab57ff831d9806f8b05696670d644caa54
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-22 02:38:15 +0500

    setup drizzle-orm in the project

--- app/utils/apiCalling.ts
+++ app/utils/apiCalling.ts
@@ -22,3 +22,16 @@ export async function detailOfSingleProduct(slug:string){
 
      return res.json();
  }
+
+
+
+export async function getProductFromIdCart(productid:string){
+const res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2024-07-14/data/query/production?query=*%5B_type+%3D%3D+%27products%27+%26%26+_id+match+%27${productid}%27+%5D%7B%0A++productname%2C%0A++++slug%2C%0A++++_id%2C%0A++++%22DescriptionText%22+%3Adescription%5B0%5D.children%5B0%5D.text%2C%0A++++price%2C%0A++++image%2C%0A++++producttype%0A%7D`)
+
+if(!res.ok){
+  return "Error"
+}
+
+return res.json();
+
+}
