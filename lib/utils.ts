cc973c5341c888a83ab927b628f26d142820321d
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-23 23:56:49 +0500

    added Shadcn Toaster component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,6 @@
-import { z } from "zod";
-export const validateUserId = z.object({
-    userid: z.string()
-  });
-  export const validateDelete = z.object({
-    userid:z.string(),
-    productid:z.string()
-  })
-  export const validatePOST = z.object({
-    userid:z.string(),
-    productid:z.string(),
-    quantity:z.number()
-  })
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
